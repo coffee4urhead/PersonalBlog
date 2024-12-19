a0913d3b3bf4b1c70aca158c5d8dc7e477aa3d02
Mihail <dark1982soul@gmail.com>
2024-12-20 01:08:32 +0200

    updating server logic

--- server/server.mjs
+++ server/server.mjs
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors"
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
+app.use(cors());
+app.use(express.json());
+
+app.get("/user/logged", (req, res) => {
+    const userLoggedIn = req.user ? true : false;
+    res.json({ loggedIn: userLoggedIn });
 });
 
 app.listen(3000, () => {
